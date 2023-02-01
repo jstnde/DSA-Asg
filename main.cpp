94c404983a16332afda3393533c271ce4b911223
jstnde <nahcnitsuj@gmail.com>
2023-02-01 23:27:06 +0800

    update main -Changes by huixin

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include "UserDictionary.h"
-#include "Forum.h"
 
 using namespace std;
 
-User* guestMenu(UserDictionary *users);
+User* mainMenu(UserDictionary *users);
 int printMenu(string mArray[], int menuLength);
 User* registerUser(UserDictionary *users);
 User* login(UserDictionary *users);
-Topic* topicMenu(Forum *topics);
+void guestMenu();
+void userMenu();
+User* userProfile(UserDictionary *users);
+void topicMenu();
+void createTopic();
 void postMenu();
-Topic* createTopic(Forum *topics);
 
 int main() {
     cout <<  "----------------------"<< endl;
@@ -19,20 +21,18 @@ int main() {
 
     //TODO: init users with text file(?)
     UserDictionary users;
-    Forum topics;
 
-    User *user = guestMenu(&users);
-    Topic *topic = topicMenu(&topics);
+    User *user = mainMenu(&users);
     cout << (user == nullptr ? "guest" : user->username) << endl;
 
     return 0;
 }
 
-User* guestMenu(UserDictionary *users) {
-    string guestMenu[] = {"1. Register", "2. Log in", "3. Continue as Guest" };
+User* mainMenu(UserDictionary *users) {
+    string guestMenu[] = {"1. Register", "2. Log in", "3. Continue as Guest", "Press CTRL + C to exit" };
     while (true) {
         User *user = nullptr;
-        int option = printMenu(guestMenu, 3);
+        int option = printMenu(guestMenu, 4);
         switch (option) {
             case 1:
                 cout <<  "Register";
@@ -97,37 +97,106 @@ User* login(UserDictionary *users) {
     return user;
 }
 
-void topicMenu(Forum *forum, User *user) {
-    string topicMenu[] = {"1. View Topics", "2.Create Topic"};
-    int option = printMenu(topicMenu, 2);
-    string title;
-    while (true) {
+void guestMenu() {
+    string guestMenu[] = {"1. View Topics", "2. Search", "3. Exit"};
+    int option = printMenu(guestMenu, 3);
+    switch (option) {
+        case 1:
+            cout << "Display Topics Menu";
+            break;
+        case 2:
+            cout << "Display Search Menu";
+            break;
+        case 3:
+            cout << "Exit to Main Menu";
+
+            break;
+        default:
+            cout << "Please enter a valid option";
+            break;
+    }
+}
+
+void userMenu() {
+    string userMenu[] = {"1. View User Info", "2. View Topics", "3. Search", "4.Logout"};
+    int option = printMenu(userMenu, 4);
+    switch (option) {
+        case 1:
+            cout << "Display User Profile";
+            break;
+        case 2:
+            cout << "Display Topics Menu";
+            break;
+        case 3:
+            cout << "Display Search Options";
+            break;
+        case 4:
+            cout << "Bring to main menu";
+            break;
+        default:
+            cout << "Please enter a valid input" << endl;
+            break;
+    }
+}
+
+User* userProfile(UserDictionary *user) {
+    cout << "Display Current User's Username";
+    string userProfileMenu[] = {"1. View All Created Topics", "2. View All Created Posts", "3. Back"};
+    int option = printMenu(userProfileMenu,3);
+    switch (option) {
+        case 1:
+            cout << "Display all topics created by user";
+            break;
+        case 2:
+            cout << "Display all posts created by user";
+            break;
+        case 3:
+            cout << "Return to User Menu";
+            break;
+        default:
+            cout << "Please enter a valid input" << endl;
+            break;
+    }
+}
+
+void topicMenu() {
+    string topicMenu[] = {"1. View All Topics","2. Create Topic", "3. Search Topics", "4. Back"};
+    int option = printMenu(topicMenu, 4);
+    //TODO: make it loop
+//    while (true) {
         switch (option) {
             case 1:
                 cout << "Display topics";
                 break;
             case 2:
                 cout << "Creating new topic";
-                createTopic(forum);
+                createTopic();
+                break;
+            case 3:
+                cout << "Search Topics";
+                break;
+            case 4:
+                cout << "Back to User Menu or Guest Menu";
+                // Check if user ==
                 break;
             default:
                 cout << "Please enter a valid option" << endl;
                 break;
 //        }
     }
-    return NULL;
 }
 
-Topic* createTopic(Forum *topics) {
+void createTopic() {
     string title;
 
     cout << endl << "Enter the topic title: ";
     cin >> title;
 
-    topics->insert(title);
+    //Add topic
     cout << "Topic Created" << endl;
-    return NULL;
 }
+
+
 
 void postMenu() {
     string postMenu[11];
